7c807c3d57b6f305f35af686dc1a416b5f209232
Roozbeh-web <roozbeh279@gmail.com>
2022-08-24 14:32:46 +0430

    add another column to the rate table

--- database/migrations/2022_08_24_081450_create_rates_table.php
+++ database/migrations/2022_08_24_081450_create_rates_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('leader_id')->constrained('users');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
