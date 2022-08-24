36be5b7bf069596763ac53616234b70f8bffbc35
Roozbeh-web <roozbeh279@gmail.com>
2022-08-24 14:19:41 +0430

    configure message migration

--- database/migrations/2022_08_24_081153_create_messages_table.php
+++ database/migrations/2022_08_24_081153_create_messages_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('messaged_user_id')->constrained('users');
+            $table->string('body');
             $table->timestamps();
         });
     }
