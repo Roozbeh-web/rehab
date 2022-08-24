ec11242cca4256c9f2b6991fb827e0733c65304a
Roozbeh-web <roozbeh279@gmail.com>
2022-08-24 16:06:17 +0430

    add identity code column to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->integer('phone');
+            $table->string('identity_code');
             $table->string('type');
             $table->string('password');
             $table->rememberToken();
