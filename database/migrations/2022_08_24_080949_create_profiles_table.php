25fd98f74a22f588fd9687b5c9189a3c04e6ac4e
Roozbeh-web <roozbeh279@gmail.com>
2022-08-24 14:07:41 +0430

    configure profile migration

--- database/migrations/2022_08_24_080949_create_profiles_table.php
+++ database/migrations/2022_08_24_080949_create_profiles_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('image');
+            $table->string('bio');
+            $table->date('birth_date');
+            $table->date('quit_date')->nullable();
             $table->timestamps();
         });
     }
