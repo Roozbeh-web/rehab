25f6a1f72ae60c77dddb5137eace724a1e7ec0cf
Roozbeh-web <roozbeh279@gmail.com>
2022-08-24 14:35:55 +0430

    configure comment migration

--- database/migrations/2022_08_24_081525_create_comments_table.php
+++ database/migrations/2022_08_24_081525_create_comments_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('leader_id')->constrained('users')->onDelete('cascade');
+            $table->string('body');
             $table->timestamps();
         });
     }
