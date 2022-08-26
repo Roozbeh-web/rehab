c4c16165a68f60b0c0377f5ce24a5b89bc61a8d2
Roozbeh-web <roozbeh279@gmail.com>
2022-08-26 23:20:53 +0430

    add sign in view: create form for sign in

---
+++ resources/views/signin.blade.php
@@ -0,0 +1,17 @@
+@extends('base')
+@extends('templates.navbar')
+
+@section('content')
+    <div class="form-container">
+        <img src="{{ URL::to('/assets/images/sign-in.png') }}" class="signin-img">
+        <form action="post" class="signin-form">
+            <h1>ورود</h1>
+            <label>نام کاربری</label><br>
+            <input type="text" name="username"><br>
+            <label>رمز عبور</label><br>
+            <input type="password" name="password">
+        </form>
+    </div>
+@endsection
+
+@extends('templates.footer')
--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,10 +1,10 @@
 @section('navbar')
     <div class="nav">
         <div>
-            <a href="#"><h1 class="nav-header-txt">هم‌ترک</h1></a>
+            <a href={{ route('home') }}><h1 class="nav-header-txt">هم‌ترک</h1></a>
         </div>
         <div class="nav-link">
-            <a href="#" class="nav-link-item">ورود</a>
+            <a href={{ route('signin') }} class="nav-link-item">ورود</a>
             <a href="#" class="nav-link-item">ثبت‌نام</a>
             <a href="#" class="nav-link-item">درباره ما</a>
             <a href="#" class="nav-link-item">تماس با ما</a>
