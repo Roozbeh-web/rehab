f807df2c76783914f76688b6ee4c6e3c76f30c4e
Roozbeh-web <roozbeh279@gmail.com>
2022-08-27 10:07:26 +0430

    modify input style of sign in form / fix typo in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <img src="{{ URL::to('/assets/images/massage.png') }}">
         </div>
         <div class="first-txt-container">
-            <h1 class="first-haeder-txt">باهم اعتیاد را ریشه کن کنیم</h1>
+            <h1 class="first-haeder-txt">باهم، اعتیاد را ریشه کن کنیم</h1>
             <p class="first-subtxt">هم‌ترک سامانه‌ی ترک اعتیاد به شیوه‌ی راهنما و مددجو. این سامانه کاملا رایگان بوده و رسالتش قدمی هرچند کوچک در راستای بهبود جامعه است.</p>
             <a href="#" class="first-btn">ثبت‌نام</a>
         </div>
