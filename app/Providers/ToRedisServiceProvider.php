517cb687a4f4e947e086fbfeaf474214ff1bb378
Roozbeh-web <roozbeh279@gmail.com>
2022-09-04 16:43:27 +0430

    create redis provider and add it to the app providers

---
+++ app/Providers/ToRedisServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Drug;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\ServiceProvider;
+
+class ToRedisServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(Drug $drug)
+    {
+    if(!Redis::get('drugs')){
+        //
+    }
+    }
+}
