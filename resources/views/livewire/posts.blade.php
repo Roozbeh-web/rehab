92fc7eaaa2310017d6042fd0b8318b9c1e5ef3c7
Roozbeh-web <roozbeh279@gmail.com>
2022-09-11 10:44:35 +0430

    add code to post component view

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,3 +1,18 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}سشب
+<div class="post-container">
+    @foreach ($posts as $post)
+        <div class="card-container">
+            <div class="image-container">
+                <img src="storage/{{$post->image}}" alt="post-img" class="post-img">
+            </div>
+            <div class="title-container">
+                <h1>{{$post->title}}</h1>
+            </div>
+            <div class="body-container">
+                <p>{{$post->body}}</p>
+            </div>
+            <div class="post-detail-container">
+                <p>{{$post->created_at}}</p>
+            </div>
+        </div>
+    @endforeach
 </div>
