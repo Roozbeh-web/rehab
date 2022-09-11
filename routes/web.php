a53cb80adaa06173b9abec22aa0f7540f9795ae1
Roozbeh-web <roozbeh279@gmail.com>
2022-09-11 09:47:13 +0430

    add name for posts route

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::group(['middleware'=>['auth']], function(){
 
     Route::get('/edit-profile', [ProfileController::class, 'getEditProfile'])->name('edit-profile');
     
-    Route::get('/posts', [PostController::class, 'getPosts']);
+    Route::get('/posts', [PostController::class, 'getPosts'])->name('posts');
 
     Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 });
