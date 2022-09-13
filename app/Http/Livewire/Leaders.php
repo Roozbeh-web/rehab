94cae0f794ac4ef026987d690f7f1d95d38229cd
Roozbeh-web <roozbeh279@gmail.com>
2022-09-13 17:29:17 +0430

    create leader livewire component

---
+++ app/Http/Livewire/Leaders.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Leaders extends Component
+{
+    public function render()
+    {
+        return view('livewire.leaders');
+    }
+}
