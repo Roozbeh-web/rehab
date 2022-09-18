1637be846b0ab5bcaae78ef8f53296913161ea78
Roozbeh-web <roozbeh279@gmail.com>
2022-09-18 17:02:41 +0430

    Revert "change directory"
    This reverts commit 33beeaca1ea5a0202a8e84123d2d6bad8d7d587e.

--- app/Http/Livewire/Leaders.php
+++ app/Http/Livewire/Leaders.php
@@ -12,13 +12,6 @@ class Leaders extends Component
     public $leaderId;
     public $requests;
 
-    public $search;
-    public $city;
-
-    public $rules = [
-        'search' => '',
-    ];
-
     public function sendRequest($leaderId){
         Request::create([
             'helpseeker_id' => auth()->id(),
@@ -31,35 +24,11 @@ class Leaders extends Component
         Request::where('leader_id', $leaderId)->where('helpseeker_id', auth()->id())->delete();
     }
 
-    public function update(){
-    }
-    
     public function render()
     {   
+        $province = auth()->user()->province;
+        $this->leaders = User::where('type', 'leader')->where('province', $province)->get();
         $this->requests = Request::where('helpseeker_id', auth()->id())->get();
-        if(!empty($this->search)){
-            $searchs = explode(' ', $this->search);
-            // $this->leaders = User::where('first_name', 'LIKE', '%'.$this->search.'%')
-            //                         ->orWhere('last_name', 'LIKE', '%'.$this->search.'%')                    
-            //                         ->orWhere('username', 'LIKE', '%'.$this->search.'%')
-            //                         ->orWhere('province', 'LIKE', '%'.$this->search.'%')
-            //                         ->orWhere('city', 'LIKE', '%'.$this->search.'%')
-            //                         ->where('type', 'leader')->get();
-            foreach($searchs as $search){
-                $this->leaders = User::where(function ($query) use($search){
-                    $query->where('first_name', 'like', '%' . $search . '%')
-                    ->orWhere('last_name', 'LIKE', '%'.$search.'%')                    
-                    ->orWhere('username', 'LIKE', '%'.$search.'%')
-                    ->orWhere('province', 'LIKE', '%'.$search.'%')
-                    ->orWhere('city', 'LIKE', '%'.$search.'%');
-                })->where('type', 'leader')
-                ->get();
-            }
-        }
-        else{
-            $province = auth()->user()->province;
-            $this->leaders = User::where('type', 'leader')->where('province', $province)->get();
-        }
         return view('livewire.leaders');
     }
 }
--- resources/views/livewire/leaders.blade.php
+++ resources/views/livewire/leaders.blade.php
@@ -1,75 +1,67 @@
 <div>
-    <div class="leader-search-container">
-        <label>جستجو</label>
-        <input type="search" wire:model="search">
-    </div>
     <div class="warn-container">
             <span>توجه: </span>
             <p> وقتی یک راهنما درخواست شما را قبول کند دیگر نمیتوانید لیست راهنما ها را مشاهده کنید مگر ارتباط خود را با راهنمایتان قطع کنید. </p>
     </div>
-    @if ($leaders)
-        @foreach ($leaders as $leader)
-            <div class="leader-container">
-                <div class="leader-card-container">
-                        <div class="leader-card-col">
-                            <div class="leader-card-row1">
-                                <div class="leader-img-container">
-                                    <img src="storage/{{$leader->profile->image}}" alt="leader-img">
-                                </div>
-                                <div class="leader-bio-container">
-                                    <p>{{$leader->profile->bio}}</p>
-                                </div>
-                                
+    @foreach ($leaders as $leader)
+        <div class="leader-container">
+            <div class="leader-card-container">
+                    <div class="leader-card-col">
+                        <div class="leader-card-row1">
+                            <div class="leader-img-container">
+                                <img src="storage/{{$leader->profile->image}}" alt="leader-img">
                             </div>
+                            <div class="leader-bio-container">
+                                <p>{{$leader->profile->bio}}</p>
+                            </div>
+                            
                         </div>
-                        <div class="leader-card-col">
-                            <div class="leader-card-row">
-                                <div class="leader-username-container">
-                                    <span><h1>{{$leader->username}}</h1></span>
-                                </div>
-                                <div class="leader-name-container">
-                                    <span><p>{{$leader->first_name}} {{$leader->last_name}}</p></span>
-                                </div>
-                                <div class="leader-quit-container">
-                                    @php
-                                        $quitDate = new DateTime($leader->profile->quit_date);
-                                        $now = new DateTime();
-                                        $interval = $now->diff($quitDate)->y;
+                    </div>
+                    <div class="leader-card-col">
+                        <div class="leader-card-row">
+                            <div class="leader-username-container">
+                                <span><h1>{{$leader->username}}</h1></span>
+                            </div>
+                            <div class="leader-name-container">
+                                <span><p>{{$leader->first_name}} {{$leader->last_name}}</p></span>
+                            </div>
+                            <div class="leader-quit-container">
+                                @php
+                                    $quitDate = new DateTime($leader->profile->quit_date);
+                                    $now = new DateTime();
+                                    $interval = $now->diff($quitDate)->y;
 
-                                    @endphp
-                                    <span><p>مدت پاکی: {{$interval}} سال</p></span>
-                                </div>
-                                <div class="leader-loc-container">
-                                    <p>استان: {{$leader->province}}</p>
-                                    <p>شهر: {{$leader->city}}</p>
-                                </div>
-                                <div >
-                                    @php
-                                        $cancelBtnCreated = false;
-                                    @endphp
-                                    @if($requests->all())
-                                        @foreach ($requests as $request)
-                                            @if ($leader->id === $request->leader_id)
-                                                <button wire:click="cancelRequest({{$leader->id}})" class="btn leader-cancel-btn">لغو درخواست</button>
-                                                @php
-                                                    $cancelBtnCreated = true;   
-                                                @endphp
-                                            @endif
+                                @endphp
+                                <span><p>مدت پاکی: {{$interval}} سال</p></span>
+                            </div>
+                            <div class="leader-loc-container">
+                                <p>استان: {{$leader->province}}</p>
+                                <p>شهر: {{$leader->city}}</p>
+                            </div>
+                            <div >
+                                @php
+                                    $cancelBtnCreated = false;
+                                @endphp
+                                @if($requests->all())
+                                    @foreach ($requests as $request)
+                                        @if ($leader->id === $request->leader_id)
+                                            <button wire:click="cancelRequest({{$leader->id}})" class="btn leader-cancel-btn">لغو درخواست</button>
+                                            @php
+                                                $cancelBtnCreated = true;   
+                                            @endphp
+                                        @endif
 
-                                        @endforeach
-                                            @if(!$cancelBtnCreated)
-                                                <button wire:click="sendRequest({{$leader->id}})" class="btn leader-request-btn">ارسال درخواست</button>
-                                            @endif
-                                    @else
-                                        <button wire:click="sendRequest({{$leader->id}})" class="btn leader-request-btn">ارسال درخواست</button>
-                                    @endif
-                                </div>
+                                    @endforeach
+                                        @if(!$cancelBtnCreated)
+                                            <button wire:click="sendRequest({{$leader->id}})" class="btn leader-request-btn">ارسال درخواست</button>
+                                        @endif
+                                @else
+                                    <button wire:click="sendRequest({{$leader->id}})" class="btn leader-request-btn">ارسال درخواست</button>
+                                @endif
                             </div>
                         </div>
                     </div>
                 </div>
-        @endforeach
-    </div>
-    @endif
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" defer></script>
-<script src="https://github.com/KayvanMazaheri/ir-city-select/releases/download/v0.2.0/ir-city-select.min.js" defer></script>
+            </div>
+    @endforeach
+</div>
