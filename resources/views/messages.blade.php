7272e9cc7d772ec8d365f8d97a21f36e33d09b96
Roozbeh-web <roozbeh279@gmail.com>
2022-09-19 11:26:08 +0430

    create messages blade view

---
+++ resources/views/messages.blade.php
@@ -0,0 +1,11 @@
+@extends('base')
+@extends('templates.dashNav')
+
+@section('content')
+@livewireStyles
+    <livewire:messages />
+@livewireScripts
+@endsection
+
+
+@extends('templates.dashFoot')
