e8b0320b14b5effa1866f390cad4bcea0c595b9e
Roozbeh-web <roozbeh279@gmail.com>
2022-09-19 10:22:36 +0430

    modify helpseeker resource and collection

--- app/Http/Resources/SpecificHelpseekerResource.php
+++ app/Http/Resources/SpecificHelpseekerResource.php
@@ -14,6 +14,19 @@ class SpecificHelpseekerResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->helpseeker->id,
+            'first_name' => $this->helpseeker->first_name,
+            'last_name' => $this->helpseeker->last_name,
+            'username' => $this->helpseeker->username,
+            'avatar' => $this->helpseeker->profile->image,
+            'birth_date' => $this->helpseeker->profile->birth_date,
+            'bio' => $this->helpseeker->profile->bio,
+            'province' => $this->helpseeker->province,
+            'city' => $this->helpseeker->city,
+            'drugs' => $this->helpseeker->drugs->toArray(),
+            'phone' => $this->helpseeker->phone,
+            'email' => $this->helpseeker->email,
+        ];
     }
 }
