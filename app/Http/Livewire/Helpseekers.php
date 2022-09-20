ca223e74cbb9893f38d47d722fbd374ec6ff03bf
Roozbeh-web <roozbeh279@gmail.com>
2022-09-20 10:24:55 +0430

    add remove messages functionality when a leader end the relation with helpseeker

--- app/Http/Livewire/Helpseekers.php
+++ app/Http/Livewire/Helpseekers.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Http\Resources\SpecificHelpseekerResource;
+use App\Models\Message;
 use App\Models\Request;
 use Livewire\Component;
 
@@ -18,8 +19,20 @@ class Helpseekers extends Component
         $request = Request::where('helpseeker_id', $helpseekerId)->where('leader_id', auth()->id());
         
         if($status === 'cancel'){
+            $id = auth()->id();
+
+            $sentMessages = Message::where('messaged_user_id', $id)->where('user_id', $helpseekerId);
+            $recivedMessages = Message::where('user_id', $id)->where('messaged_user_id', $helpseekerId);
+            
+            $messages = $sentMessages->union($recivedMessages);
+
+            if($messages->exists()){
+                $messages->delete();
+            }
+
             $request->delete();
         }
+        
         else{
             $request->update(['status'=>'block']);
         }
